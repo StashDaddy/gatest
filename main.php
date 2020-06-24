f95e583473c6bbc11dc42891bfb56f26667d4c3a
Chris Evans <evans.chris.b@gmail.com>
2020-06-24 10:15:27 -0500

    Added check for assignment during if (x = 1); enabled phpmd, phpcs for check comparisons

--- main.php
+++ main.php
@@ -1,5 +1,7 @@
 <?php
 require_once("test-working.php");
 require_once("test-syntaxerror.php");
+require_once("test-syntaxerror2.php");
+require_once("test-syntaxerror3.php");
 
 echo "Done" . PHP_EOL;
---
+++ test-syntaxerror3.php
@@ -0,0 +1,5 @@
+<?php
+    $x = 1;
+    if ($x = 2) {
+        echo "This can't be reached";
+    }
